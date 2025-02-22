278523aa6eb4c69630d8ed77d53eb2a3c9c2e372
João Pedro <jaumpredo8068@gmail.com>
2025-02-22 01:34:53 -0300

    Implementação do menu Cadastro de Parceiro

---
+++ src/commands/menuCadastroParceiro.js
@@ -0,0 +1,36 @@
+class MenuParceiro {
+    static async execute(userInput, state) {
+        const lowerCaseUserInput = userInput.toLowerCase();
+
+        if (lowerCaseUserInput === 'q') {
+            state.currentMenu = 'main';
+            return "⬅ Voltando ao menu principal...";
+        }
+
+        if (state.currentMenu === 'parceiro') {
+            switch (lowerCaseUserInput) {
+                case 's':
+                    return "💼 **Cadastro de Parceiro:**\n Acesse: *www.example.com/partner/new_partner* e faça seu cadastro!";
+                case 'n':
+                    this.resetState(state);
+                    return "⬅ Voltando ao menu principal...";
+                default:
+                    return "⚠️ Opção inválida. Por favor, escolha uma opção válida:\n\n" + this.getMenu();
+            }
+        }
+    }
+    
+    static resetState(state) {
+        Object.assign(state, {
+            currentMenu: 'main',
+            hasShownWelcome: true, // Mantém como true para evitar dupla mensagem
+            selectedCity: null,
+            previousInput: null
+        });
+    }
+
+    static getMenu() {
+        return "Deseja cadastrar um parceiro? [s/n]: ";
+    }
+}
+module.exports = MenuParceiro;
